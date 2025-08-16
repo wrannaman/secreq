92b6569e031f8e0a031144d3bcc46266518b7a4b
wrannaman <andrewpierno@gmail.com>
2025-08-16 12:57:57 -0700

    add crisp

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import { ToastProvider } from "@/components/toast-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { AuthProvider } from "@/hooks/use-auth";
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,6 +39,13 @@ export default function RootLayout({ children }) {
             <Toaster />
           </ToastProvider>
         </ThemeProvider>
+        <Script
+          id="crisp-chat"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="259b8605-003a-48b1-a6b0-563fb4a1aca3";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`,
+          }}
+        />
       </body>
       <Analytics />
     </html>
